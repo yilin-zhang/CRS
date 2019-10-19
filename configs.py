8de631fe7e5329bcd3286478c766a8c7eb48d53a
Yilin Zhang <yzhang0129@outlook.com>
2019-10-19 16:08:54 -0400

    remove __pycache__, add configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+# All possible chord names
+CHORD_SEQ_1 = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+CHORD_SEQ_2 = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+# Chord types
+SYM_MAJ = 'maj'
+SYM_MIN = 'min'
+TYPE_SEQ = [SYM_MAJ, SYM_MIN]
+N_TYPES = len(TYPE_SEQ)
--- utils.py
+++ utils.py
@@ -1,15 +1,5 @@
 import numpy as np
-from mcgill_parser import McGillParser
-
-# All possible chord names
-CHORD_SEQ_1 = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-CHORD_SEQ_2 = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
-
-# Chord types
-SYM_MAJ = 'maj'
-SYM_MIN = 'min'
-TYPE_SEQ = [SYM_MAJ, SYM_MIN]
-N_TYPES = len(TYPE_SEQ)
+from configs import *
 
 def chord_to_onehot(chord):
     ''' Convert a chord to a one-hot array. It only supports triads.
