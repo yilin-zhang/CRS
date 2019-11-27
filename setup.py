1c66c5529e28ddb7433336ca1e7b6fcb506e0f71
Yilin Zhang <yzhang0129@outlook.com>
2019-11-27 12:07:01 -0500

    Fix a bug of creating directory

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 '''
 Set up the program, create necessary files.
 '''
+import os
 
 from chord_recommendation.mcgill_parser import McGillParser
 from chord_recommendation.markov_model import MarkovModel
 from chord_recommendation.configs import MARKOV_ORDER
 from chord_recommendation.utils import chords_to_nums, transpose_chord_nums
-from config import MARKOV_MODEL_PATH
+from config import CACHE_PATH, MARKOV_MODEL_PATH
 
 # Train the Markov model, save the model to `cache` directory
 parser = McGillParser()
@@ -18,4 +19,8 @@ with markov.train_batch():
         chord_nums = chords_to_nums(chords)
         chord_seqs = [chord_nums] + transpose_chord_nums(chord_nums)
         markov.feed_seqs(chord_seqs)
+
+if not os.path.exists(CACHE_PATH):
+    os.makedirs(CACHE_PATH)
+
 markov.serialize(MARKOV_MODEL_PATH)
