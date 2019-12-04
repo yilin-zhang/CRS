7f47c74b916cc25c695e258de15262d825be5695
rishi <rishikesh.daoo@gatech.edu>
2019-12-04 18:17:43 -0500

    Added HPS implementation

--- chord_detection/chromagram.py
+++ chord_detection/chromagram.py
@@ -58,25 +58,48 @@ def compute_stft(filepath):
 
     return X, fs, t
 
-def extract_pitch_chroma(X, fs, tfInHz):
-   block_length = X.shape[0]
-   num_blocks = X.shape[1]
-   pitch_chroma = np.zeros((12, block_length), dtype=np.int16)
+def HPS(X, fs, order):
+    freqRange = int((len(X[0]) - 1) / order)
+    # print len(X)
+    # print X.shape
+    f0 = np.zeros((1, len(X)))
+    hps = np.zeros((len(X), freqRange))
+    freqSpread = np.linspace(0, fs / 2, len(X[0]))
+    for h in range(len(X)):
+        for i in range(freqRange):
+            multiplier = 1
+            for j in range(1, order + 1):
+                multiplier = multiplier * (X[h, i * j])
+            hps[h, i] = multiplier
+            if max(hps[h, :]) > 10 ** 10:
+                hps[h, :] = hps[h, :] / max(hps[h, :])
+    return hps
 
-   Y = np.abs(X) ** 2
+def extract_pitch_chroma(X, fs, tfInHz, baseline_ver = 2):
 
-   # Need to calculate pitch chroma from C3 to B5 --> 48 to 83
-   lower_bound = 48
-   upper_bound = 84
+    if baseline_ver == 1:
+        Y = np.abs(X) ** 2
+    elif baseline_ver == 2:
+        Y = HPS(X, fs, 2)
+    else:
+        Y = HPS(X, fs, 2)
 
-   k = np.arange(1, (block_length+1))
-   k_freq = k * fs / (2 * (block_length-1))
+    # Need to calculate pitch chroma from C3 to B5 --> 48 to 83
+    lower_bound = 48
+    upper_bound = 84
 
-   irange = (upper_bound-lower_bound)
+    block_length = Y.shape[0]
+    num_blocks = Y.shape[1]
+    pitch_chroma = np.zeros((12, block_length), dtype=np.int16)
 
-   logfreq_X = np.zeros([irange, num_blocks])
+    k = np.arange(1, (block_length+1))
+    k_freq = k * fs / (2 * (block_length-1))
 
-   for n, i in enumerate(range(lower_bound, upper_bound)):
+    irange = (upper_bound-lower_bound)
+
+    logfreq_X = np.zeros([irange, num_blocks])
+
+    for n, i in enumerate(range(lower_bound, upper_bound)):
 
        midi_pitch_lower = 2 ** (((i - 0.5) - 69) / 12) * tfInHz
        midi_pitch_upper = 2 ** (((i + 0.5) - 69) / 12) * tfInHz
@@ -86,19 +109,19 @@ def extract_pitch_chroma(X, fs, tfInHz):
        logfreq_X[n, :] = Y[k[mask], :].sum(axis=0)
 
 
-   pitch_chroma = np.zeros((12, logfreq_X.shape[1]))
-   p = np.arange(48,84)
-   for c in range(12):
+    pitch_chroma = np.zeros((12, logfreq_X.shape[1]))
+    p = np.arange(48,84)
+    for c in range(12):
        mask = (p % 12) == c
        pitch_chroma[c, :] = logfreq_X[mask, :].sum(axis=0)
 
-   idx = [7,8,9,10,11,0,1,2,3,4,5,6]
-   # idx = [8,7,6,5,4,3,2,1,0,11,10,9]
+    idx = [7,8,9,10,11,0,1,2,3,4,5,6]
+    # idx = [8,7,6,5,4,3,2,1,0,11,10,9]
 
-   pitch_chroma = pitch_chroma[idx, :]
+    pitch_chroma = pitch_chroma[idx, :]
 
-   l2norm = np.linalg.norm(pitch_chroma, ord=2, axis=0)
-   l2norm[l2norm == 0] = 1
-   pitch_chroma /= l2norm
+    l2norm = np.linalg.norm(pitch_chroma, ord=2, axis=0)
+    l2norm[l2norm == 0] = 1
+    pitch_chroma /= l2norm
 
-   return pitch_chroma
+    return pitch_chroma
